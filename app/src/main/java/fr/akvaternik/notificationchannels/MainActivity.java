0caa596f8d33ac34c24c270e1c86b6b050d06878
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2017-04-04 22:26:46 +0200

    Add channel groups

--- app/src/main/java/fr/akvaternik/notificationchannels/MainActivity.java
+++ app/src/main/java/fr/akvaternik/notificationchannels/MainActivity.java
@@ -2,6 +2,7 @@ package fr.akvaternik.notificationchannels;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
@@ -14,10 +15,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String ARTICLE_CHANNEL_ID = "article_channel";
+    public static final String PERSONAL_ARTICLE_CHANNEL_ID = "personal_article_channel";
+    public static final String BUSINESS_ARTICLE_CHANNEL_ID = "business_article_channel";
+
+    public static final String PERSONAL_GROUP_ID = "personal_group";
+    public static final String BUSINESS_GROUP_ID = "business_group";
 
     private NotificationManager notificationManager;
     private int notificationId = 0;
@@ -38,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         notifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendNotification();
+                sendNotification(ARTICLE_CHANNEL_ID);
             }
         });
 
@@ -46,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         configurationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                displayChannelConfiguration();
+                displayChannelConfiguration(ARTICLE_CHANNEL_ID);
             }
         });
 
@@ -56,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToChannelSettings();
+                goToChannelSettings(ARTICLE_CHANNEL_ID);
             }
         });
 
@@ -64,41 +71,66 @@ public class MainActivity extends AppCompatActivity {
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                deleteChannel();
+                deleteChannel(ARTICLE_CHANNEL_ID);
+            }
+        });
+
+        Button notifyPersonalGroupButton = (Button) findViewById(R.id.notify_personal_group_button);
+        notifyPersonalGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification(PERSONAL_ARTICLE_CHANNEL_ID);
             }
         });
     }
 
     private void configureNotifications() {
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        createNotificationChannel();
+        createNotificationChannelGroups();
+        createNotificationChannels();
     }
 
-    private void createNotificationChannel() {
-        NotificationChannel articleChannel = new NotificationChannel(ARTICLE_CHANNEL_ID, getString(R.string.article_channel_name), NotificationManager.IMPORTANCE_DEFAULT);
+    private void createNotificationChannelGroups() {
+        NotificationChannelGroup personalGroup = new NotificationChannelGroup(PERSONAL_GROUP_ID, getString(R.string.personal_group_name));
+        NotificationChannelGroup businessGroup = new NotificationChannelGroup(BUSINESS_GROUP_ID, getString(R.string.business_group_name));
 
+        List<NotificationChannelGroup> groups = Arrays.asList(personalGroup, businessGroup);
+
+        notificationManager.createNotificationChannelGroups(groups);
+    }
+
+    private void createNotificationChannels() {
+        NotificationChannel articleChannel = new NotificationChannel(ARTICLE_CHANNEL_ID, getString(R.string.article_channel_name), NotificationManager.IMPORTANCE_DEFAULT);
         articleChannel.enableLights(true);
         articleChannel.setLightColor(Color.BLUE);
         articleChannel.enableVibration(true);
         articleChannel.setVibrationPattern(new long[]{100, 100});
 
-        notificationManager.createNotificationChannel(articleChannel);
+        NotificationChannel personalArticleChannel = new NotificationChannel(PERSONAL_ARTICLE_CHANNEL_ID, getString(R.string.article_channel_name), NotificationManager.IMPORTANCE_DEFAULT);
+        personalArticleChannel.setGroup(PERSONAL_GROUP_ID);
+
+        NotificationChannel businessArticleChannel = new NotificationChannel(BUSINESS_ARTICLE_CHANNEL_ID, getString(R.string.article_channel_name), NotificationManager.IMPORTANCE_DEFAULT);
+        businessArticleChannel.setGroup(BUSINESS_GROUP_ID);
+
+        List<NotificationChannel> channels = Arrays.asList(articleChannel, personalArticleChannel, businessArticleChannel);
+
+        notificationManager.createNotificationChannels(channels);
     }
 
-    private void sendNotification() {
+    private void sendNotification(String channelId) {
         Notification notification = new Notification.Builder(this)
                 .setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle(getString(R.string.new_article_title))
                 .setContentText(getString(R.string.new_article_text))
-                .setChannel(ARTICLE_CHANNEL_ID)
+                .setChannel(channelId)
                 .build();
 
         notificationManager.notify(notificationId, notification);
         notificationId++;
     }
 
-    private void displayChannelConfiguration() {
-        NotificationChannel channel = notificationManager.getNotificationChannel(ARTICLE_CHANNEL_ID);
+    private void displayChannelConfiguration(String channelId) {
+        NotificationChannel channel = notificationManager.getNotificationChannel(channelId);
         if (channel != null) {
             CharSequence name = channel.getName();
             int importance = channel.getImportance();
@@ -114,14 +146,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void goToChannelSettings() {
+    private void goToChannelSettings(String channelId) {
         Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
-        intent.putExtra(Settings.EXTRA_CHANNEL_ID, ARTICLE_CHANNEL_ID);
+        intent.putExtra(Settings.EXTRA_CHANNEL_ID, channelId);
         intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
         startActivity(intent);
     }
 
-    private void deleteChannel() {
-        notificationManager.deleteNotificationChannel(ARTICLE_CHANNEL_ID);
+    private void deleteChannel(String channelId) {
+        notificationManager.deleteNotificationChannel(channelId);
     }
 }
